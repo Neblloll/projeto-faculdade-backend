9ce32560d2a33ac18df7102ea5ec972438800b48
Matheus Barros <matheusbarros1911@gmail.com>
2023-05-26 21:00:55 -0300

    Criado Projeto Faculdade

---
+++ src/main/java/br/com/neblloll/projeto_faculdade/ProjetoFaculdadeApplication.java
@@ -0,0 +1,13 @@
+package br.com.neblloll.projeto_faculdade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFaculdadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFaculdadeApplication.class, args);
+	}
+
+}
